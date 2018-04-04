75b6c1cffb0561657887d979638a8aedfe183e15
Kyle Robbertze <krobbertze@gmail.com>
2018-04-04 22:43:37 +0200

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo $ANSIBLE_VAULT_PASSWORD > ~/ansible_vault_password.txt
+mkdir ~/.ssh/
+chmod 700 ~/.ssh
+echo $SSH_KEY > ~/.ssh/id_rsa
+
+git clone https://gitlab.com/paddatrapper/dotfiles ~/dotfiles
+cd ~/dotfiles
+stow ansible
+
+cd ~/
+git clone https://gitlab.com/paddatrapper/ansible
+cd ~/ansible
+
+ansible-playbook --vault-password-file=~/ansible_vault_password.txt
+
+rm ~/ansible_vault_password.txt
