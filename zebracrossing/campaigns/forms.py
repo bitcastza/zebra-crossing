152c96d4e8ce0c6393ebc27a075ef39aa7988add
Kyle Robbertze <krobbertze@gmail.com>
2018-05-07 21:15:21 +0200

    display time slots as checkbox list

--- zebracrossing/campaigns/forms.py
+++ zebracrossing/campaigns/forms.py
@@ -5,10 +5,11 @@ from .models import BookingSheet, Campaign
 class BookingSheetForm(forms.ModelForm):
     class Meta:
         model = BookingSheet
-        fields = ['material', 'ad_type', 'start_date', 'end_date']
+        fields = ['material', 'ad_type', 'start_date', 'end_date', 'time_slots']
         widgets = {
             'start_date': forms.DateInput(attrs={'class': 'date-input'}),
             'end_date': forms.DateInput(attrs={'class': 'date-input'}),
+            'time_slots': forms.CheckboxSelectMultiple(),
         }
 
 class CampaignForm(forms.ModelForm):
