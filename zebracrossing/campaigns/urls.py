b341784cc49246fd332126b52584dc1950e606c0
Kyle Robbertze <krobbertze@gmail.com>
2019-03-04 11:27:43 +0200

    Hide booking sheet time slot view until supported

--- zebracrossing/campaigns/urls.py
+++ zebracrossing/campaigns/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('add_campaign/', views.add_campaign, name='add_campaign'),
     path('<int:pk>/', views.CampaignView.as_view(), name='detail'),
     path('<int:campaign_id>/add_booking/', views.add_booking, name='add_booking'),
-    path('booking/<int:pk>/', views.BookingView.as_view(), name='show_booking'),
-    path('booking/<int:booking_id>/sheet/', views.download_booking_sheet, name='download_booking_sheet'),
+    # TODO: When time slots are supported, include this url
+    #path('booking/<int:pk>/', views.BookingView.as_view(), name='show_booking'),
+    path('booking/<int:booking_id>/', views.download_booking_sheet, name='download_booking_sheet'),
 ]
