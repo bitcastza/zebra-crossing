73fc145458bdbaccdb2bcc6401d0df8a2ef4e2b5
Kyle Robbertze <kyle@paddatrapper.com>
2022-05-09 21:50:52 +0200

    fix: support django 4.x

--- zebracrossing/campaigns/apps.py
+++ zebracrossing/campaigns/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class CampaignsConfig(AppConfig):
-    name = 'campaigns'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'zebracrossing.campaigns'
+    verbose_name = 'Zebra Crossing campaign management'
