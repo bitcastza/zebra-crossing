7cf68f8abdf0ecfda4769d55151f3908e5791c62
Kyle Robbertze <kyle@paddatrapper.com>
2022-08-18 09:38:02 +0200

    fix: split settings into module
    Fixes: #3

--- zebracrossing/settings/common.py
+++ zebracrossing/settings/common.py
@@ -19,14 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "h5rz)p$&9=k5bhgjo1@h+kip5jn&nt8r-f_cba_t!v7m=7f9sx"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 # Application definition
 
 INSTALLED_APPS = [
---
+++ zebracrossing/settings/dev.py
@@ -0,0 +1,9 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "h5rz)p$&9=k5bhgjo1@h+kip5jn&nt8r-f_cba_t!v7m=7f9sx"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
